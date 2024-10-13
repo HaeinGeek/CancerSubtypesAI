58e6ceef9b1ebc9ae7afab17e38e8311da97187a
HaeinGeek <50579220+HaeinGeek@users.noreply.github.com>
2024-10-13 19:38:50 +0900

    Create sequence_fetcher.py

---
+++ processing/sequence_fetcher.py
@@ -0,0 +1,8 @@
+from processing.uniprot_api import UniProtAPI
+from processing.pdb_api import PBDAPI
+from processing.ncbi_api import NCBIAPI
+from processing.ensembl_api import EnsemblAPI
+
+class SequenceFetcher(UniProtAPI, PBDAPI, NCBIAPI, EnsemblAPI):
+    """여러 API를 통합해 단백질 서열을 가져오는 클래스."""
+    pass

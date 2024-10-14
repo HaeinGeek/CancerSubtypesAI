b78e69c925901cab2488ed416beea120b48cede4
HaeinGeek <1995khi@gmail.com>
2024-10-14 21:58:42 +0900

    Update encode_mutations.py

--- scripts/encode_mutations.py
+++ scripts/encode_mutations.py
@@ -5,39 +5,43 @@ import os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 
-from encoder.mutation_encoder import process_mutations
+from encoder.mutation_encoder import classify_and_aggregate_mutations
+from encoder.mutation_encoder import parse_multiple_mutations
 import pandas as pd
 
 def main():
     import os
 
     # CSV 파일 읽기
-    input_filepath = 'data/train.csv'  # 실제 파일 경로로 수정하세요
+    input_filepath = 'data/train.csv'
     if not os.path.exists(input_filepath):
         print(f"Input file '{input_filepath}' not found.")
         return
 
     train_df = pd.read_csv(input_filepath)
 
-    train_type = train_df.copy()
-    train_position = train_df.copy()
+    # 데이터 변환 및 중복 제거
+    melted_df = train_df.melt(id_vars=['ID', 'SUBCLASS'], var_name='gene', value_name='mutation_str')
+    unique_mutations_df = melted_df[['gene', 'mutation_str']].drop_duplicates(subset=['gene', 'mutation_str'])
 
-    for column in train_df.columns:
-        if column not in ['ID', 'SUBCLASS']:  # ID와 SUBCLASS 열 제외
-            # process_mutations 함수가 변이 종류와 변이 위치 두 개의 값을 반환하므로, 이를 각각 저장
-            train_type[column], train_position[column] = zip(*train_df[column].apply(process_mutations))
+    # 변이 인코딩 데이터프레임 생성
+    mutation_encoding_df = unique_mutations_df.copy()
+
+    # 변이 종류
+    mutation_encoding_df['type'] = unique_mutations_df['mutation_str'].apply(classify_and_aggregate_mutations)
+
+    # 변이 아미노산 및 변이 위치 처리
+    mutation_encoding_df['origin'], mutation_encoding_df['position'], mutation_encoding_df['mutant'] = zip(
+        *unique_mutations_df['mutation_str'].apply(parse_multiple_mutations)
+        )
+    
+    # 변이 위치의 개수
+    mutation_encoding_df['mut_num'] = mutation_encoding_df['position'].apply(lambda x: len(x) if isinstance(x, list) else 0)
+
 
     # 결과 저장
-    train_type.to_csv('data/processed/train_mut_type_encoding.csv', index=False)
-    train_position.to_csv('data/processed/train_mut_position_encoding.csv', index=False)
-
-    # 변이 위치의 개수 인코딩
-    df_pos_len = train_position.copy()
-    for column in df_pos_len.columns:
-        if column not in ['ID', 'SUBCLASS']:  # ID와 SUBCLASS 열 제외
-            df_pos_len[column] = df_pos_len[column].apply(lambda x: len(x) if isinstance(x, list) else 0)
-
-    df_pos_len.to_csv('data/processed/train_mut_position_len.csv', index=False)
+    output_filepath = 'data/processed/train_mutation_encoding.csv'
+    mutation_encoding_df.to_csv(output_filepath, index=False)
 
     print("Encoding completed successfully.")
 

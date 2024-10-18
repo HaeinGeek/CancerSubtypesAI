2528fe1dea3049646eab0eaef31e14a5d81a486a
HaeinGeek <1995khi@gmail.com>
2024-10-18 16:27:02 +0900

    Create __init__.py

---
+++ embedding/__init__.py
@@ -0,0 +1,13 @@
+from embedding.protbert_embedding import (
+    extract_data_for_embedding,
+    load_protbert,
+    extract_full_embedding,
+    process_and_save_embeddings
+)
+
+__all__ = [
+    'extract_data_for_embedding',
+    'load_protbert',
+    'extract_full_embedding',
+    'process_and_save_embeddings'
+    ]

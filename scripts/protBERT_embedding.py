bdee84ec3f966d652271ea92aca68683d5eeebaf
HaeinGeek <1995khi@gmail.com>
2024-10-18 13:30:48 +0900

    Update protBERT_embedding.py

--- scripts/protBERT_embedding.py
+++ scripts/protBERT_embedding.py
@@ -9,29 +9,20 @@ def setup_environment():
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     sys.path.append(project_root)
 
-def load_data():
+def load_full_feature_data():
     mut_seq_df = pd.read_csv('data/processed/mutant_seq_unique.csv')
     mut_encoding_df = pd.read_csv('data/processed/train_mutation_encoding.csv')
     
     for column in ['origin', 'position', 'mutant']:
         mut_encoding_df[column] = mut_encoding_df[column].apply(ast.literal_eval)
     
-    return mut_seq_df.merge(mut_encoding_df, on=['gene', 'mutation_str'], how='left')
+    merged_df =  mut_seq_df.merge(mut_encoding_df, on=['gene', 'mutation_str'], how='left')
 
-def process_data(merged_df):
     merged_df['status_prot'] = 0
     merged_df.loc[merged_df['type'] == 'Frameshift', 'status_prot'] = 1
     merged_df.loc[(merged_df['type'] == 'Complex_mutation') & (merged_df['mutant'].apply(lambda x: 'fs' in x)), 'status_prot'] = 1
 
-    merged_df_prot = merged_df.loc[(merged_df.type != 'WT') & 
-                                   (merged_df.type != 'Silent_Missense') & 
-                                   (merged_df.type != 'Silent_Nonsense') & 
-                                   (merged_df.status_prot == 0)]
-
-    wt_prot = merged_df_prot.drop_duplicates(subset=['isoform_id'])[['gene', 'isoform_id', 'wt_seq']]
-    mut_prot = merged_df_prot.drop_duplicates(subset=['isoform_id', 'mutation_str'])[['gene', 'mutation_str', 'isoform_id', 'mut_seq']]
-
-    return wt_prot, mut_prot
+    return merged_df
 
 def extract_embedding(sequence, tokenizer, model):
     inputs = tokenizer(sequence, return_tensors='pt', max_length=1024, padding=True, truncation=True)
@@ -54,12 +45,20 @@ def extract_and_save_embeddings(prot_data, tokenizer, model, output_file):
             embedding_dict[isoform_id] = extract_embedding(sequence, tokenizer, model)
     
     torch.save(embedding_dict, output_file)
-    print(f"임베딩이 {output_file}에 저장되었습니다.")
+    print(f"Embeddings were saved to {output_file}")
 
 def main():
     setup_environment()
-    merged_df = load_data()
-    wt_prot, mut_prot = process_data(merged_df)
+    df = load_full_feature_data()
+    
+    df_prot = df.loc[(df.type != 'WT') &
+                     (df.type != 'Silent_Missense') &
+                     (df.type != 'Silent_Nonsense') &
+                     (df.status_prot == 0)
+                     ]
+
+    wt_prot = df_prot.drop_duplicates(subset=['isoform_id'])[['gene', 'isoform_id', 'wt_seq']]
+    mut_prot = df_prot.drop_duplicates(subset=['isoform_id', 'mutation_str'])[['gene', 'mutation_str', 'isoform_id', 'mut_seq']]
 
     tokenizer = BertTokenizer.from_pretrained('Rostlab/prot_bert')
     model = BertModel.from_pretrained('Rostlab/prot_bert')
@@ -67,7 +66,7 @@ def main():
     extract_and_save_embeddings(wt_prot, tokenizer, model, 'data/processed/wt_embedding_tensor.pt')
     extract_and_save_embeddings(mut_prot, tokenizer, model, 'data/processed/mut_embedding_tensor.pt')
 
-    print("모든 임베딩 추출 및 저장이 완료되었습니다.")
+    print("All embeddings have been extracted and saved.")
 
 if __name__ == "__main__":
     main()

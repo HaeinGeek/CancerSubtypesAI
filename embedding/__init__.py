25da00127d03813bb79d3c5e6904e8cdfba25f69
HaeinGeek <1995khi@gmail.com>
2024-10-29 22:24:13 +0900

    Create __init__.py

---
+++ embedding/__init__.py
@@ -0,0 +1,19 @@
+from embedding.protbert_embedding import (
+    extract_data_for_embedding,
+    load_protbert,
+    apply_pooling,
+    extract_embeddings,
+    process_embeddings,
+    # save_embeddings_to_zip,
+    # load_compressed_embeddings
+)
+
+__all__ = [
+    'extract_data_for_embedding',
+    'load_protbert',
+    'apply_pooling',
+    'extract_embeddings',
+    'process_embeddings',
+    # "save_embeddings_to_zip",
+    # 'load_compressed_embeddings'
+    ]

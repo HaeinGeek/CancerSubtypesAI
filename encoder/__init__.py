de3d709f355ffeeaa22b9d9e44204033ebd3cbc1
HaeinGeek <1995khi@gmail.com>
2024-10-29 22:24:49 +0900

    Create __init__.py

---
+++ encoder/__init__.py
@@ -0,0 +1,18 @@
+from encoder.mutation_encoder import (
+    get_amino_acid_info,
+    parse_mutation,
+    parse_multiple_mutations,
+    classify_single_mutation,
+    classify_and_aggregate_mutations,
+    calculate_amino_acid_diff,
+    process_mutation_features
+)
+
+__all__ = ['get_amino_acid_info',
+    'parse_mutation',
+    'parse_multiple_mutations',
+    'classify_single_mutation',
+    'classify_and_aggregate_mutations',
+    'calculate_amino_acid_diff',
+    'process_mutation_features'
+    ]

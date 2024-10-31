e4dcf19a85c68cf6128ec1e02fd609330a6f9d32
HaeinGeek <1995khi@gmail.com>
2024-10-31 17:25:38 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,17 @@
 from utils.utils import(
     setup_environment,
     load_logger,
-    load_full_feature_data
+    load_full_feature_data,
+    load_full_seq_data,
+    download_file,
+    txt_to_list
 )
 
 __all__ = [
     'setup_environment',
     'load_logger',
-    'load_full_feature_data'
+    'load_full_feature_data',
+    'load_full_seq_data',
+    'download_file',
+    'txt_to_list'
 ]
